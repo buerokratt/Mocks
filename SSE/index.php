6a922be97b33e1eb93b3478f4a24791312ab1d7c
Rainer Türner <90505798+turnerrainer@users.noreply.github.com>
2023-02-09 09:22:08 +0200

    Update index.php

--- SSE/index.php
+++ SSE/index.php
@@ -15,7 +15,8 @@ while (true) {
   $counter--;
 
   if (!$counter) {
-    echo 'data: {"headers":{"Pragma":["no-cache"],"Cache-Control":["no-cache"],"Expires":["Wed, 31 Dec 1969 23:59:59 GMT"],"Content-Type":["application/json"]},"body":{"data":{"forwarding_validation":[{"id":"a8cd5b6c-6f6f-7fb9-f03b-a2215863b711","chatid":"f7fe8858-e419-4573-bd37-7b8bdba7d112","content":"Boti vastus '.rand().'","event":"","authorId":"demo-sse","authorTimestamp":"'.$curDate.'","authorFirstName":"","authorLastName":"","authorRole":"chatbot","rating":"","created":"'.$curDate.'","updated":"'.$curDate.'"},{"id":"e0e46b42-ba20-1126-bc98-b64290f8cf4c","chatid":"f7fe8858-e419-4573-bd37-7b8bdba7d112","content":"Mock-SSE vastus","event":"","authorId":"demo","authorTimestamp":"'.$curDate.'","authorFirstName":"","authorLastName":"","authorRole":"chatbot","rating":"","created":"'.$curDate.'","updated":"'.$curDate.'"}]},"error":null},"statusCode":"OK","statusCodeValue":200}' . "\n\n";
+    #echo 'data: {"headers":{"Pragma":["no-cache"],"Cache-Control":["no-cache"],"Expires":["Wed, 31 Dec 1969 23:59:59 GMT"],"Content-Type":["application/json"]},"body":{"data":{"forwarding_validation":[{"id":"a8cd5b6c-6f6f-7fb9-f03b-a2215863b711","chatid":"f7fe8858-e419-4573-bd37-7b8bdba7d112","content":"Boti vastus '.rand().'","event":"","authorId":"demo-sse","authorTimestamp":"'.$curDate.'","authorFirstName":"","authorLastName":"","authorRole":"chatbot","rating":"","created":"'.$curDate.'","updated":"'.$curDate.'"},{"id":"e0e46b42-ba20-1126-bc98-b64290f8cf4c","chatid":"f7fe8858-e419-4573-bd37-7b8bdba7d112","content":"Mock-SSE vastus","event":"","authorId":"demo","authorTimestamp":"'.$curDate.'","authorFirstName":"","authorLastName":"","authorRole":"chatbot","rating":"","created":"'.$curDate.'","updated":"'.$curDate.'"}]},"error":null},"statusCode":"OK","statusCodeValue":200}' . "\n\n";
+    echo 'data: ' .rand(1,9999999999999). "\n\n";
     $counter = rand(1, 4);
   }
 
